e5728752d945af403af7701692a6605042edffcf
Alejandro Hellin Gil <ahellingil@gmail.com>
2024-04-10 16:44:39 +0200

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# powershell-snippets
+# PowerShell Code Snippets
+
+This is my personal code snippets for the powershell scripts I usually use.
+
+| File                         | Summary                                                                                                                          |
+|------------------------------|--------------------------------------------------------------------------------------------------------------------------------|
+| [logPing.ps1](logPing.ps1)                  | script to improve the ping utility, logging the datetime of each ping and be able to know when it fails. It tests hostname/ip connection throw a ping every x milliseconds and log the datetime of each ping. |
+| [openExcelAndPassPassword.ps1](openExcelAndPassPassword.ps1) | Open a Microsoft Excel Workbook that is protected by a password passing the password directly.                                 |
--- logPing.ps1
+++ logPing.ps1
@@ -1,8 +1,14 @@
 #
-# PowerShell script to test hostname/ip connection throw a ping every x milliseconds and log the datetime of each ping.
-# Use: testping -target <host> -ms <ms>
+# PowerShell script to improve the ping utility, 
+# logging the datetime of each ping and and be able to know when it fails.
+# It tests the hostname/ip connection throw a ping every x milliseconds 
+# and log the datetime of each ping.
+#
+# Use: testping -target <host> -ms <ms> -count 
 #   -target <host>: <host> is the hostname or IP to ping.
-#   -ms <ms>: <ms> is the milliseconds between each test (1000 ms by default).
+#   -ms <number>: <number> is the milliseconds between each test (1000 ms by default).
+#   -count <number>: <number> is the number of pings that will be sent. 
+#       A value of 0 (the default) means infinite pings until the user force the stop of the script.
 #
 # Returns:
 #   Lines in the format: <hour>;<host>;<result>;<message>. Where:
@@ -12,15 +18,21 @@
 #         If <result> = ERROR, <message> shows the error message that occurrs.
 #
 #   Examples: 
-#     testping                                                 # output the ping to www.google.com every 1000 milliseconds
-#     testping -target www.example.com                         # output the ping to www.example.com every 1000 ms
-#     testping -target www.myserver.com -ms 600 > results.csv  # log the ping to www.myserver.com every 600 ms to a file results.csv
+#     # 1. output continually the ping to www.google.com every 1000 milliseconds
+#     testping
+#
+#     # 2. output the 4 pings to www.example.com every 1000 ms
+#     testping -target www.example.com -count 4                
+#
+#     # 3. log continually the ping to www.myserver.com every 600 ms to a file results.csv
+#     testping -target www.myserver.com -ms 600 > results.csv  
 #
 # 2024-04-10 - ahellingil - Draft 1.0
 #
 param(
     [string]$target,
-    [int]$ms
+    [int]$ms,
+    [int]$count
 )
 if (-not $target) {
     $target = "www.google.com"
@@ -28,8 +40,11 @@ if (-not $target) {
 if (-not $ms) {
     $ms = 1000
 }
+if (-not $count) {
+    $count = 0
+}
 Write-Output "Hour; Host; Result; Message"
-for (;;) {
+for ($i = 1; $i -le $count -or $count -eq 0; $i++) {
     try {
         $dateTime = Get-Date
         $pingResult = Test-Connection -ComputerName $target -Count 1 -ErrorAction Stop
@@ -38,6 +53,8 @@ for (;;) {
         $status = "ERROR; $_"
     }
     Write-Output "$dateTime; $target; $status"
-    Start-Sleep -Milliseconds $ms    
+    if (-not ($i -eq $count)) {
+        Start-Sleep -Milliseconds $ms
+    }
 }
 
