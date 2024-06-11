5335ba493a377f6d1e798dfa255a7f0684047f54
Alejandro Hellin Gil <ahellingil@gmail.com>
2024-06-11 13:28:59 +0200

    listFilesRecursively.ps1 examples

--- listFilesRecursively.ps1
+++ listFilesRecursively.ps1
@@ -1,12 +1,19 @@
 <# 
 .DESCRIPTION
  PowerShell script to list recursively files with the full path included.
- You can specify the path or 
+ You can specify the path to search or the files filter to apply.
 
 .SYNOPSIS
    ./listFilesRecursively.ps1 [-path <path>] [-filter <filter>]
    .PARAMETER <path>: specifies the path to search files. If is omited, path "." is applied.
    .PARAMETER <filter>: specifies the filter you want to apply (i.e. *.txt).
+
+.EXAMPLE
+     # 1. List all files of the current folder:
+     ./listFilesRecursively.ps1
+
+     # 2. List markdown files (*.md) of the projects folder:
+     ./listFilesRecursively.ps1 -path c:\projects -filter *.md
 
 .NOTES
  Author: ahellingil
